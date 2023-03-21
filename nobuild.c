f46a2cb76e8b792ebf64f7bc7cd40b7e5171a2c5
NotCarl <cmoroni04@gmail.com>
2023-03-21 14:32:22 +0100

    Added new features to the nobuild™ branch

--- nobuild.c
+++ nobuild.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #define NOBUILD_IMPLEMENTATION
 #include "./nobuild.h"
 
@@ -9,9 +10,26 @@ void compile(void){
     INFO("Ora esegui exec_this - eventualmente con flag degug");
 }
 
+void debug(void){
+    compile();
+    CMD("./exec_this","debug");
+    RM("exec_this");
+}
+
 int main(int argc, char **argv){
     GO_REBUILD_URSELF(argc, argv);
     INFO("Hi there, we are creating this file in order to let you build only one file that will compile everything dor you!");
     compile();
+    const char *program = shift_args(&argc, &argv);
+    if (argc>0) {
+        const char *subcmd = shift_args(&argc, &argv);
+        if (strcmp(subcmd, "run")==0) {
+            CMD("./exec_this");
+        } else if (strcmp(subcmd, "debug")==0) {
+            debug();
+        }
+    } else {
+        INFO("Ran without arguments");
+    }
     return 0;
 }
--- runDebug.sh
+++
@@ -1,5 +0,0 @@
-make
-clear
-printf "\n\n"
-./exec_this debug
-rm exec_this
