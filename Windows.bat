4162d8ff1ddfd3dcb9a7f20fcdd676170c7ca212
NotCarl <cmoroni04@gmail.com>
2023-03-22 14:05:00 +0100

    Tested the nobuild file, therfore we eliminated the useless file

--- Windows.bat
+++
@@ -1,4 +0,0 @@
-gcc -o exec_this.exe -I src/headers/headers txt.c src/headers/implementation/NewFunctions.c src/headers/implementation/NewInteractions.c src/headers/implementation/interface.c
-cls
-exec_this.exe debug
-del exec_this.exe
